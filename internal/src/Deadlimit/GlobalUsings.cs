54b81e863fc324a4a22d5ef7ca8219c4967c589d
Oleg Knyazev <down.limit@gmail.com>
2026-08-22 20:34:13 +0300

    Make desktop app global usings explicit

--- internal/src/Deadlimit/GlobalUsings.cs
+++ internal/src/Deadlimit/GlobalUsings.cs
@@ -1,1 +1,7 @@
+global using System;
+global using System.Collections.Generic;
+global using System.Drawing;
+global using System.IO;
+global using System.Linq;
 global using System.Text.Json;
+global using System.Windows.Forms;
