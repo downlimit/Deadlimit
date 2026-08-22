ad6e238d7247bcd07804ba2f86d8da9140aee468
Oleg Knyazev <down.limit@gmail.com>
2026-08-22 21:21:23 +0300

    Expose prepare and compile action in desktop app

--- internal/src/Deadlimit/Program.cs
+++ internal/src/Deadlimit/Program.cs
@@ -9,6 +9,9 @@ internal static class Program
     {
         Application.EnableVisualStyles();
         Application.SetCompatibleTextRenderingDefault(false);
-        Application.Run(new MainForm());
+
+        var form = new MainForm();
+        BuildFeature.Attach(form);
+        Application.Run(form);
     }
 }
