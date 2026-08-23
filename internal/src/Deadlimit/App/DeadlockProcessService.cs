4223ba14e4a95c429c38054411519b1c6c9ad232
Oleg Knyazev <down.limit@gmail.com>
2026-08-23 21:04:07 +0300

    Handle running Deadlock before retail VPK deployment

---
+++ internal/src/Deadlimit/App/DeadlockProcessService.cs
@@ -0,0 +1,114 @@
+namespace Deadlimit.App;
+
+internal static class DeadlockProcessService
+{
+    private static readonly string[] ProcessNames = ["deadlock", "project8"];
+
+    public static bool IsRunning()
+    {
+        foreach (var processName in ProcessNames)
+        {
+            var processes = System.Diagnostics.Process.GetProcessesByName(processName);
+            try
+            {
+                if (processes.Any(process => !process.HasExited))
+                {
+                    return true;
+                }
+            }
+            finally
+            {
+                foreach (var process in processes)
+                {
+                    process.Dispose();
+                }
+            }
+        }
+
+        return false;
+    }
+
+    public static async Task<bool> CloseAsync(CancellationToken cancellationToken = default)
+    {
+        var processes = GetRunningProcesses();
+        try
+        {
+            foreach (var process in processes)
+            {
+                cancellationToken.ThrowIfCancellationRequested();
+
+                try
+                {
+                    if (!process.HasExited)
+                    {
+                        process.CloseMainWindow();
+                    }
+                }
+                catch (InvalidOperationException)
+                {
+                }
+            }
+
+            if (await WaitUntilStoppedAsync(TimeSpan.FromSeconds(4), cancellationToken))
+            {
+                return true;
+            }
+
+            foreach (var process in processes)
+            {
+                cancellationToken.ThrowIfCancellationRequested();
+
+                try
+                {
+                    if (!process.HasExited)
+                    {
+                        process.Kill(entireProcessTree: true);
+                    }
+                }
+                catch (InvalidOperationException)
+                {
+                }
+                catch (System.ComponentModel.Win32Exception)
+                {
+                }
+            }
+
+            return await WaitUntilStoppedAsync(TimeSpan.FromSeconds(5), cancellationToken);
+        }
+        finally
+        {
+            foreach (var process in processes)
+            {
+                process.Dispose();
+            }
+        }
+    }
+
+    private static List<System.Diagnostics.Process> GetRunningProcesses()
+    {
+        var result = new List<System.Diagnostics.Process>();
+        foreach (var processName in ProcessNames)
+        {
+            result.AddRange(System.Diagnostics.Process.GetProcessesByName(processName));
+        }
+        return result;
+    }
+
+    private static async Task<bool> WaitUntilStoppedAsync(
+        TimeSpan timeout,
+        CancellationToken cancellationToken)
+    {
+        var deadline = DateTime.UtcNow + timeout;
+        while (DateTime.UtcNow < deadline)
+        {
+            cancellationToken.ThrowIfCancellationRequested();
+            if (!IsRunning())
+            {
+                return true;
+            }
+            await Task.Delay(200, cancellationToken);
+        }
+
+        return !IsRunning();
+    }
+}
