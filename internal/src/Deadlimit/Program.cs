e90f572df20036a667a645f21328ab3500c985c0
Oleg Knyazev <down.limit@gmail.com>
2026-08-23 17:56:40 +0300

    Use branded icon for the main window and taskbar

--- internal/src/Deadlimit/Program.cs
+++ internal/src/Deadlimit/Program.cs
@@ -1,17 +1,31 @@
+using System.Drawing;
 using Deadlimit.App;
 
 namespace Deadlimit;
 
 internal static class Program
 {
+    private static readonly Icon AppIcon = LoadAppIcon();
+
     [STAThread]
     private static void Main()
     {
         Application.EnableVisualStyles();
         Application.SetCompatibleTextRenderingDefault(false);
 
-        var form = new MainForm();
+        var form = new MainForm
+        {
+            Icon = AppIcon,
+        };
         BuildFeature.Attach(form);
         Application.Run(form);
+    }
+
+    private static Icon LoadAppIcon()
+    {
+        using var stream = typeof(Program).Assembly.GetManifestResourceStream("Deadlimit.AppIcon.ico")
+            ?? throw new InvalidOperationException("Embedded Deadlimit application icon was not found.");
+        using var source = new Icon(stream);
+        return (Icon)source.Clone();
     }
 }
