dacc6e461e0addffddd3fc8a929f3b707a5ffbbd
Oleg Knyazev <down.limit@gmail.com>
2026-08-23 23:48:14 +0300

    Attach retail hero catalog UI

--- internal/src/Deadlimit/Program.cs
+++ internal/src/Deadlimit/Program.cs
@@ -29,6 +29,7 @@ internal static class Program
         };
         BuildFeature.Attach(form);
         ProjectLibraryFeature.Attach(form);
+        HeroCatalogFeature.Attach(form);
         UiTheme.ApplyCustomPalette(form, settings.UiTheme);
         form.Shown += (_, _) => form.BeginInvoke((Action)(() => form.ActiveControl = null));
         Application.Run(form);
